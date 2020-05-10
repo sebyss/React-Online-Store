7d4ba4729a5ad71f756a757929d3f9045aef8b58
Sebastian Miucin <sebastian.miucin96@e-uvt.ro>
2020-05-10 23:03:29 +0300

    added filters functionality

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,68 +1,77 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { Menu, Checkbox } from "antd";
-import { connect } from "react-redux";
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { Menu, Checkbox } from 'antd'
+import { connect } from 'react-redux'
 
-const { SubMenu } = Menu;
+const { SubMenu } = Menu
 
-const Products = ({ categories }) => {
-  const [loading, setLoading] = useState(true);
+const getFilters = (categories, selectedCategory) => {
+  var filtersList = []
+  var filtersNames = []
 
-  useEffect(() => {
-    if (categories) {
-      setLoading(false);
-    }
-  }, [categories]);
-
-  function onChange(checkedValues) {
-    console.log("checked = ", checkedValues);
-  }
-
-  if (loading) {
-    return <p>loading...</p>;
-  }
-
-  var filtersList = [];
-  var filtersNames = [];
-
-  const arrayFiltersList = categories.map((e) => e.filtersList);
+  const filtersForSelectedCategory = categories.filter((e) => e.name === selectedCategory)
+  const arrayFiltersList = filtersForSelectedCategory.map((e) => e.filtersList)
 
   arrayFiltersList.map((e) =>
     Object.keys(e).map((item) =>
-      Object.values(e[item]).map((filter) => {
-        filtersList = [
-          ...filtersList,
-          { value: filter, label: filter, category: item },
-        ];
-      })
+      Object.values(e[item]).map(
+        (filter) =>
+          (filtersList = [...filtersList, { value: filter, label: filter, category: item }])
+      )
     )
-  );
+  )
 
   arrayFiltersList.map((e) =>
     Object.keys(e).map((item) => (filtersNames = [...filtersNames, item]))
-  );
+  )
+
+  return { filtersList, filtersNames }
+}
+
+const Products = ({ categories }) => {
+  const [loading, setLoading] = useState(true)
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [activeFilters, setActiveFilters] = useState({})
+
+  useEffect(() => {
+    if (categories) {
+      setLoading(false)
+    }
+  }, [categories])
+
+  const addActiveFilters = (checkedValues, filterType) => {
+    setActiveFilters({ ...activeFilters, ...activeFilters.filterType, [filterType]: checkedValues })
+  }
+
+  if (loading) {
+    return <p>loading...</p>
+  }
+  console.log(activeFilters)
+  const filters = getFilters(categories, selectedCategory)
 
   return (
     <Page>
       <SideFiltersContainer>
         <StyledMenu
-          defaultSelectedKeys={["1"]}
-          defaultOpenKeys={["sub1", "sub2"]}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1', 'sub2']}
           mode="inline"
           inlineCollapsed={false}
         >
           <StyledSubmenu key="sub1" title="Categorii">
-            {categories.map((e, id) => (
-              <StyledMenuItem key={id}>{e.name}</StyledMenuItem>
+            {categories.map((e) => (
+              <StyledMenuItem key={e.name} onClick={(e) => setSelectedCategory(e.key)}>
+                {e.name}
+              </StyledMenuItem>
             ))}
           </StyledSubmenu>
 
-          {filtersNames.map((e) => (
+          {filters.filtersNames.map((e) => (
             <StyledSubmenu key={e} title={e}>
               <div>
                 <StyledCheckBoxGroup
-                  options={filtersList.filter((i) => i.category === e)}
-                  onChange={onChange}
+                  options={filters.filtersList.filter((filter) => filter.category === e)}
+                  onChange={(checkedValues) => addActiveFilters(checkedValues, e)}
                 />
               </div>
             </StyledSubmenu>
@@ -73,28 +82,28 @@ const Products = ({ categories }) => {
         <TopFiltersContainer />
       </ContentContainer>
     </Page>
-  );
-};
+  )
+}
 
 const mapStateToProps = (state) => ({
   categories: state.categories.list,
-});
+})
 
-export default connect(mapStateToProps, null)(Products);
+export default connect(mapStateToProps, null)(Products)
 
 const Page = styled.div`
   padding-top: 30px;
   min-height: 100vh;
   display: flex;
   min-height: 100vh;
-`;
+`
 
 const SideFiltersContainer = styled.div`
   width: 250px;
   height: 100vh;
   display: flex;
   flex-direction: column;
-`;
+`
 
 const ContentContainer = styled.div`
   width: 100%;
@@ -102,17 +111,17 @@ const ContentContainer = styled.div`
   display: flex;
   background-color: blue;
   margin-left: 15px;
-`;
+`
 
 const TopFiltersContainer = styled.div`
   width: 100%;
   height: 40px;
   background-color: green;
-`;
+`
 
 const StyledMenu = styled(Menu)`
   border-right: 0px;
-`;
+`
 
 const StyledSubmenu = styled(SubMenu)`
   border-radius: 25px;
@@ -132,14 +141,14 @@ const StyledSubmenu = styled(SubMenu)`
   &:focus {
     border-radius: 25px;
   }
-`;
+`
 
 const StyledMenuItem = styled(Menu.Item)`
   border-radius: 25px;
-`;
+`
 
 const StyledCheckBoxGroup = styled(Checkbox.Group)`
   display: flex;
   flex-direction: column;
   margin-left: 30px;
-`;
+`

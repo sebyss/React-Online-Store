9cf7d804e0669c415557d48c05e583362df68262
sebastian.miucin96@gmail.com <sebastian.miucin96@gmail.com>
2020-05-10 19:07:02 +0300

    created dynamic menus

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,44 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { Menu, Checkbox } from 'antd'
-import { connect } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Menu, Checkbox } from "antd";
+import { connect } from "react-redux";
 
-const { SubMenu } = Menu
+const { SubMenu } = Menu;
 
 const Products = ({ categories }) => {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (!Object.keys(categories).length) {
-      setLoading(false)
-      
+    if (categories) {
+      setLoading(false);
     }
-  }, [categories])
-
-
-
-  const options = [
-    { label: 'Apple', value: 'Apple' },
-    { label: 'Pear', value: 'Pear' },
-    { label: 'Orange', value: 'Orange' },
-  ]
+  }, [categories]);
 
   function onChange(checkedValues) {
-    console.log('checked = ', checkedValues)
+    console.log("checked = ", checkedValues);
   }
 
   if (loading) {
-    return <p>loading...</p>
+    return <p>loading...</p>;
   }
 
-  console.log(categories)
+  var filtersList = [];
+  var filtersNames = [];
+
+  const arrayFiltersList = categories.map((e) => e.filtersList);
+
+  arrayFiltersList.map((e) =>
+    Object.keys(e).map((item) =>
+      Object.values(e[item]).map((filter) => {
+        filtersList = [
+          ...filtersList,
+          { value: filter, label: filter, category: item },
+        ];
+      })
+    )
+  );
+
+  arrayFiltersList.map((e) =>
+    Object.keys(e).map((item) => (filtersNames = [...filtersNames, item]))
+  );
 
   return (
     <Page>
       <SideFiltersContainer>
         <StyledMenu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1', 'sub2']}
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1", "sub2"]}
           mode="inline"
           inlineCollapsed={false}
         >
@@ -48,37 +57,44 @@ const Products = ({ categories }) => {
             ))}
           </StyledSubmenu>
 
-          <StyledSubmenu key="sub2" title="Sebitza">
-            <StyledCheckBoxGroup options={options} defaultValue={['Apple']} onChange={onChange} />
-          </StyledSubmenu>
+          {filtersNames.map((e) => (
+            <StyledSubmenu key={e} title={e}>
+              <div>
+                <StyledCheckBoxGroup
+                  options={filtersList.filter((i) => i.category === e)}
+                  onChange={onChange}
+                />
+              </div>
+            </StyledSubmenu>
+          ))}
         </StyledMenu>
       </SideFiltersContainer>
       <ContentContainer>
         <TopFiltersContainer />
       </ContentContainer>
     </Page>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => ({
   categories: state.categories.list,
-})
+});
 
-export default connect(mapStateToProps, null)(Products)
+export default connect(mapStateToProps, null)(Products);
 
 const Page = styled.div`
   padding-top: 30px;
   min-height: 100vh;
   display: flex;
   min-height: 100vh;
-`
+`;
 
 const SideFiltersContainer = styled.div`
   width: 250px;
   height: 100vh;
   display: flex;
   flex-direction: column;
-`
+`;
 
 const ContentContainer = styled.div`
   width: 100%;
@@ -86,17 +102,17 @@ const ContentContainer = styled.div`
   display: flex;
   background-color: blue;
   margin-left: 15px;
-`
+`;
 
 const TopFiltersContainer = styled.div`
   width: 100%;
   height: 40px;
   background-color: green;
-`
+`;
 
 const StyledMenu = styled(Menu)`
   border-right: 0px;
-`
+`;
 
 const StyledSubmenu = styled(SubMenu)`
   border-radius: 25px;
@@ -116,14 +132,14 @@ const StyledSubmenu = styled(SubMenu)`
   &:focus {
     border-radius: 25px;
   }
-`
+`;
 
 const StyledMenuItem = styled(Menu.Item)`
   border-radius: 25px;
-`
+`;
 
 const StyledCheckBoxGroup = styled(Checkbox.Group)`
   display: flex;
   flex-direction: column;
   margin-left: 30px;
-`
+`;
